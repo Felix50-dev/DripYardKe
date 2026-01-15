ad9acc86df0328f0ea1d495cb821e5040b255a3a
Felix50-dev <mwatufelix1@gmail.com>
2026-01-15 16:35:43 +0300

    Fix CI indentation and add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,35 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.ktlint) apply false
 }
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    extensions.configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension>("detekt") {
+        config.setFrom(files("$rootDir/detekt.yml"))
+        buildUponDefaultConfig = true
+        ignoreFailures = false  // fail CI on any violation
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    extensions.configure<KtlintExtension>("ktlint") {
+        android.set(true)
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+        filter {
+            exclude("**/ui/**")
+        }
+    }
+}
+
+
